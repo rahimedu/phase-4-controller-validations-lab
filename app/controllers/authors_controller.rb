34177bcddefb5a74faefe285ecb8e5291f4ed13a
rahimedu <edwin.duche82@gmail.com>
2022-10-01 23:11:47 -0500

    commit tree a9d19bb80beac339e3f666d3fb452f78b0da332a

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,27 @@
 class AuthorsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    
+    def show
+      author = Author.find(params[:id])
   
-  def show
-    author = Author.find(params[:id])
-
-    render json: author
-  end
-
-  def create
-    author = Author.create(author_params)
-
-    render json: author, status: :created
-  end
-
-  private
+      render json: author
+    end
   
-  def author_params
-    params.permit(:email, :name)
-  end
+    def create
+      author = Author.create!(author_params)
   
-end
+      render json: author, status: :created
+    end
+  
+    private
+    
+    def author_params
+      params.permit(:email, :name)
+    end
+    
+    def render_unprocessable_entity_response(invalid)
+      render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
+  end
+    
